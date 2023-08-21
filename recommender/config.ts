5979f108acb856c87f9a5f13d30433d20fed5d1b
Kimmo <kkpsiren@gmail.com>
2023-08-21 17:52:19 +0300

    test

--- recommender/config.ts
+++ recommender/config.ts
@@ -1,73 +1,81 @@
 export const config = {
-	ogs: ["yoginth.lens", "christina.lens", "mariariivari.lens",
-		"bradorbradley.lens", "wagmi.lens", "levychain.lens", "nicolo.lens",
-		"sasicodes.lens", "stani.lens", "davidev.lens" ],
-	localtrustStrategies: [
-		'existingConnections',
-		'f6c3m8enhancedConnections',
-		'f6c3m8col12enhancedConnections',
-		'f6c3m8col12Price',
-	],
-	pretrustStrategies: [
-		'pretrustOGs',
-		'pretrustFirstFifty',
-		'pretrustAllEqually'
-	],
-	contentStrategies: [
-		'viralPosts',
-	],
-	rankingStrategies: [{
-			name: 'followship',
-			pretrust: 'pretrustOGs',
-			localtrust: 'existingConnections',
-			alpha: 0.5
-		}, {
-			name: 'engagement',
-			pretrust: 'pretrustOGs',
-			localtrust: 'f6c3m8enhancedConnections',
-			alpha: 0.5
-		}, {
-			name: 'influencer',
-			pretrust: 'pretrustOGs',
-			localtrust: 'f6c3m8col12enhancedConnections',
-			alpha: 0.5
-		}, {
-			name: 'creator',
-			pretrust: 'pretrustOGs',
-			localtrust: 'f6c3m8col12Price',
-			alpha: 0.5
-		},
-	],
-	sqlFeedStrategies: [{
-		name: "popular",
-		feed: "viralFeedWithEngagement",
-		limit: 100,
-	}, {
-		name: "recent",
-		feed: "latestFeed",
-		limit: 100,
-	}],
-	algoFeedStrategies: [{
-		name: "recommended",
-		feed: "ml-xgb-followship",
-		limit: 100,
-	},{
-		name: "crowdsourced",
-		feed: "hubs-and-authorities",
-		limit: 100,
-	}],
-	personalFeedStrategies: [{
-		name: "following",
-		feed: "followingViralFeedWithEngagement",
-		limit: 100,
-	}],
-	personalization: {
-		globaltrust: 'followship',
-		ltStrategyName: 'existingConnections',
-		limitGlobaltrust: 100
-	},
-	content: {
-		strategy: "viralPosts",
-		limitUsers: 50,
-	},
-}
+  ogs: ["orbapp.lens", "nilesh.lens", "kipto.lens", "sankalpk.lens"],
+  localtrustStrategies: [
+    "existingConnections",
+    "f6c3m8enhancedConnections",
+    "f6c3m8col12enhancedConnections",
+    "f6c3m8col12Price",
+  ],
+  pretrustStrategies: [
+    "pretrustOGs",
+    "pretrustFirstFifty",
+    "pretrustAllEqually",
+  ],
+  contentStrategies: ["viralPosts"],
+  rankingStrategies: [
+    {
+      name: "followship",
+      pretrust: "pretrustOGs",
+      localtrust: "existingConnections",
+      alpha: 0.5,
+    },
+    {
+      name: "engagement",
+      pretrust: "pretrustOGs",
+      localtrust: "f6c3m8enhancedConnections",
+      alpha: 0.5,
+    },
+    {
+      name: "influencer",
+      pretrust: "pretrustOGs",
+      localtrust: "f6c3m8col12enhancedConnections",
+      alpha: 0.5,
+    },
+    {
+      name: "creator",
+      pretrust: "pretrustOGs",
+      localtrust: "f6c3m8col12Price",
+      alpha: 0.5,
+    },
+  ],
+  sqlFeedStrategies: [
+    {
+      name: "popular",
+      feed: "viralFeedWithEngagement",
+      limit: 100,
+    },
+    {
+      name: "recent",
+      feed: "latestFeed",
+      limit: 100,
+    },
+  ],
+  algoFeedStrategies: [
+    {
+      name: "recommended",
+      feed: "ml-xgb-followship",
+      limit: 100,
+    },
+    {
+      name: "crowdsourced",
+      feed: "hubs-and-authorities",
+      limit: 100,
+    },
+  ],
+  personalFeedStrategies: [
+    {
+      name: "following",
+      feed: "followingViralFeedWithEngagement",
+      limit: 100,
+    },
+  ],
+  personalization: {
+    globaltrust: "followship",
+    ltStrategyName: "existingConnections",
+    limitGlobaltrust: 100,
+  },
+  content: {
+    strategy: "viralPosts",
+    limitUsers: 50,
+  },
+};
